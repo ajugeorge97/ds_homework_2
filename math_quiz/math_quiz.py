d99b1680a50c10218c13e44d237f9781915eb6fb
Aju George <aju.george@fau.de>
2023-11-13 07:40:49 +0100

    refactor: fixed errors error handling docstrings added refactor

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,86 @@
 import random
 
 
-def function_A(min, max):
+def random_generator(min, max):
     """
-    Random integer.
+    Random Generator 
+    Desc: This function generates a Random integer.
+    input:
+        min: minimum value
+        max:max value
+    returns:
+        random int
+
     """
     return random.randint(min, max)
 
-
-def function_B():
+def rand_math_operation():
+    """
+    rand_math_operation
+    Desc : Generate a random math operation
+    return:
+        random choice of list['+', '-', '*']
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculate_output(value_1, value_2, operation):
+    """
+    Calculate the result of a mathematical operation between two values.
+
+    Parameters:
+        value_1 : The first operand.
+        value_2: The second operand.
+        operation(str): The mathematical operation to perform. Should be one of '+', '-', or '*'.
+
+    Returns:
+        tuple: A tuple containing the mathematical problem as a string and the calculated result.
+    """
+
+
+    problem = f"{value_1} {operation} {value_2}"
+    if operation == '+': 
+        output = value_1 + value_2
+    elif operation == '-': 
+        output = value_1 - value_2
+    else: 
+        output = value_1 * value_2
+    return problem, output
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Math quiz: 
+    An interactive python math quiz game
+    
+    """
+    score = 0
+    tries= 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(int(tries)):
+        #variables generation
+        value_1 = random_generator(1, 10)
+        value_2 = random_generator(1, 5)
+        operation = rand_math_operation()
+        #output calculation
+        problem, output = calculate_output(value_1, value_2, operation)
+        print(f"\nQuestion: {problem}")
+        try:
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+        except Exception as e:
+            print(e)
+            print("Please Check your input")
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
+        if useranswer == output:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {output}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{tries}")
 
 if __name__ == "__main__":
     math_quiz()
