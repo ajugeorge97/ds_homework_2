0ff882623ccb22417fe37732817092e2372f205d
Aju George <aju.george@fau.de>
2023-11-13 19:59:51 +0100

    fix : setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='math_quiz',
+      version='0.1',
+      description='The math quiz ',
+      url='https://github.com/ajugeorge97/ds_homework_2.git',
+      author='Aju George',
+      author_email='aju.george@fau.de',
+      license='Appache 2.0',
+      packages=['math_quiz'],
+      zip_safe=False)
